fe285715d0f46151129926c3c63b15cadf621308
Fatma Betül Güreş <60520606+betty-gures@users.noreply.github.com>
2024-09-28 11:50:40 +0200

    Assets

---
+++ Assets/SciFiAREffect/Scripts/Ro.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ro : MonoBehaviour
+{
+
+    public float F_X;
+    public float F_Y;
+    public float F_Z;
+
+
+    private void Awake()
+    {
+        F_X = F_X * 20f;
+        F_Y = F_Y * 20f;
+        F_Z = F_Z * 20f;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		transform.Rotate(new Vector3(F_X*Time.deltaTime, F_Y * Time.deltaTime, F_Z * Time.deltaTime),Space.Self);
+	}
+}
